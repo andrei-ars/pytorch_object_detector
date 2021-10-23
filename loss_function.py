89fe0f478b85f4992794ed629239f09393d594d7
Andrei <z@sp.sp>
2021-10-24 00:49:50 +0300

    10 ep

--- loss_function.py
+++ loss_function.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch import sqrt
 
 mse_loss = nn.MSELoss()
 
@@ -7,10 +8,17 @@ def bboxes_loss(logits, labels):
     #bb_square = (logits[:,:4] - labels[:,:4]) ** 2
     
     loc_sq = (logits[:,:2] - labels[:,:2]) ** 2
-    size_sq = (logits[:,2:4] - labels[:,2:4]) ** 2
+    #size_sq = (logits[:,2:4] - labels[:,2:4]) ** 2
+    size_sq = (sqrt(logits[:,2:4]) - sqrt(labels[:,2:4])) ** 2
     obj_sq = (logits[:,4] - labels[:,4]) ** 2
     #bbox_loss = torch.mean(loc_sq) + 0.1*torch.mean(size_sq) + 0.01 * torch.mean(obj_sq)
-    bbox_loss = torch.mean(loc_sq) + 0.1*torch.mean(size_sq) + 0.1 * torch.mean(obj_sq)
+    bbox_loss = torch.mean(loc_sq) + torch.mean(size_sq) + 0.1 * torch.mean(obj_sq)
+
+    print("loc_sq:", loc_sq)
+    print("size_sq:", size_sq)
+    print("obj_sq:", obj_sq)
+    print("bbox_loss:", bbox_loss)
+    
 
     #loc_sq = (logits[:,:2] - labels[:,:2]) ** 2
     #size_sq = (logits[:,2:4] - 0.1) ** 2
@@ -25,8 +33,10 @@ def bboxes_loss(logits, labels):
 
 if __name__ == "__main__":
 
-    logits = torch.tensor([[0, 0, 1, 1, 100], [1, 1, 3, 3, 100]])
-    labels = torch.tensor([[0.1, 0.1, 0.9, 0.9, 1000], [1, 1, 3.2, 3.2, 1000]])
+    #logits = torch.tensor([[0, 0, 1, 1, 100], [1, 1, 3, 3, 100]], dtype=torch.float32)
+    #labels = torch.tensor([[0.1, 0.1, 0.9, 0.9, 100], [1, 1, 3.2, 3.2, 100]], dtype=torch.float32)
+    logits = torch.tensor([[0, 0, 5, 5, 100], [0, 0, 5, 5, 100]], dtype=torch.float32)
+    labels = torch.tensor([[1, 1, 1, 1, 100], [1, 1, 1, 1, 100]], dtype=torch.float32)
 
     print(logits)
     print(labels)
