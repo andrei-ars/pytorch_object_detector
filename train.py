5fb1fa97f2dd4608cb2670c2c30050aa477070ab
Andrei <z@sp.sp>
2021-10-24 12:43:24 +0300

    10 ep

--- train.py
+++ train.py
@@ -43,7 +43,7 @@ pickle_dataset_path = "dataset_{}.pickle".format(image_width)
 
 
 #start_lr = 0.00128
-num_epochs = 50; start_lr = 0.00256; step_size = 10
+num_epochs = 100; start_lr = 0.00256; step_size = 20
 #num_epochs = 25; start_lr = 0.00256; step_size = 4
 #num_epochs = 25; start_lr = 0.00128; step_size = 5
 #num_epochs = 25; start_lr = 0.00128; step_size = 5
